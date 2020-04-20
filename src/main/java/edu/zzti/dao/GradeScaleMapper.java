597eee3c6011e87c65f9f72479ca851d1a083528
shc <1005919371@qq.com>
2020-04-20 19:35:59 +0800

    Merge branch 'master' of D:\WorkspacesIdea\graduationProject\system with conflicts.

--- src/main/java/edu/zzti/dao/GradeScaleMapper.java
+++ src/main/java/edu/zzti/dao/GradeScaleMapper.java
@@ -3,6 +3,13 @@ package edu.zzti.dao;
 import edu.zzti.bean.GradeScale;
 
 public interface GradeScaleMapper {
+
+
+    //shc
+    GradeScale findNewApplyGradeScale(Integer gsStatus);
+    //shc
+    GradeScale selectByGsStatus(Integer gsStatus);
+
     int deleteByPrimaryKey(Integer gsId);
 
     int insert(GradeScale record);
